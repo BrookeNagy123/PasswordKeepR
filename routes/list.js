aa3ee2ebd866cd7253b6e75e375b51e9f3c436ab
binding1 <jaey.pro@gmail.com>
2023-02-03 03:40:04 +0000

    fix /list routing

--- routes/list.js
+++ routes/list.js
@@ -1,14 +1,14 @@
 /*
  * All routes for Password List are defined here
- * Since this file is loaded in server.js into api/list,
- * these routes are mounted onto /api/list
+ * Since this file is loaded in server.js into /list,
+ * these routes are mounted onto /list
  */
 
 const express = require('express');
 const router  = express.Router();
-const db = require('../db/connection');
+// const db = require('../db/connection');
 
-router.get('/list', (req, res) => {
+router.get('/', (req, res) => {
   res.render('list')
 });
 
--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
-const passwordList = require('./routes/list')
+const passwordList = require('./routes/list');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -39,7 +39,7 @@ const passwordList = require('./routes/list')
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
-app.use('/;list', passwordList);
+app.use('/list', passwordList);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
