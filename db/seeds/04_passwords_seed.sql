9cd8c8c9c464250631c6b6ebc395989255768b1b
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-02-04 13:12:29 -0700

    Update to username

--- db/seeds/04_passwords_seed.sql
+++ db/seeds/04_passwords_seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('Slack', 1, 'https://slack.com', 'Lighthouse Labs Slack', 'password', 2);
-INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('Instagram', 1, 'https://www.instagram.com', 'Lighthouse Labs Instagram', 'password', 1);
-INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('LinkedIn', 2, 'https://www.linkedin.com', 'Tweeter LinkedIn', 'password', 2);
-INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('YouTube', 2, 'https://www.youtube.com/', 'Tweeter YouTube', 'password', 3);
+INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('Slack', 1, 'https://slack.com', 'Lighthouse-Labs-Slack', 'password', 2);
+INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('Instagram', 1, 'https://www.instagram.com', 'Lighthouse-Labs-Instagram', 'password', 1);
+INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('LinkedIn', 2, 'https://www.linkedin.com', 'Tweeter-LinkedIn', 'password', 2);
+INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('YouTube', 2, 'https://www.youtube.com/', 'Tweeter-YouTube', 'password', 3);
