422861380e8e6b4dd6e11f79909fc1c96028595a
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-02-04 12:10:59 -0700

    Add username for passwords

--- db/seeds/04_passwords_seed.sql
+++ db/seeds/04_passwords_seed.sql
@@ -1,3 +1,4 @@
-INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('Slack', 1, 'https://slack.com/signin#/signin', 'password', 2);
-INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('Instagram', 1, 'https://www.instagram.com/accounts/login/', 'password', 1);
-INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('LinkedIn', 1, 'https://www.linkedin.com/login', 'password', 2);
+INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('Slack', 1, 'https://slack.com', 'Lighthouse Labs Slack', 'password', 2);
+INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('Instagram', 1, 'https://www.instagram.com', 'Lighthouse Labs Instagram', 'password', 1);
+INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('LinkedIn', 2, 'https://www.linkedin.com', 'Tweeter LinkedIn', 'password', 2);
+INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('YouTube', 2, 'https://www.youtube.com/', 'Tweeter YouTube', 'password', 3);
