cb6b869f9e98cb939bd9c20bce1303ee6c3890ce
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-02-03 17:38:51 -0700

    Password seed data

---
+++ db/seeds/04_passwords_seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('Slack', 1, 'https://slack.com/signin#/signin', 'password', 2);
+INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('Instagram', 1, 'https://www.instagram.com/accounts/login/', 'password', 1);
+INSERT INTO passwords (name, vault_id, url, username, password, category_id ) VALUES ('LinkedIn', 1, 'https://www.linkedin.com/login', 'password', 2);
