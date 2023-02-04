4fc9e606860da9058a50e2ca225a9a8eb540d9ee
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-02-04 11:59:41 -0700

    Route for password generator

--- routes/generator.js
+++ routes/generator.js
@@ -1,20 +1,45 @@
 /*
- * All routes for Password List are defined here
- * Since this file is loaded in server.js into /list,
- * these routes are mounted onto /list
+ * All routes for Password Generator are defined here
+ * Since this file is loaded in server.js into /generator,
+ * these routes are mounted onto /generator
  */
 
 const express = require('express');
 const router  = express.Router();
 // const db = require('../db/connection');
 
+const includeUppercase = document.querySelector("#uppercase");
+const includeLowercase = document.querySelector("#lowercase");
+const includeNumbers = document.querySelector("#numbers");
+const includeSymbols = document.querySelector("#symbols");
+const passLength = document.querySelector("#length");
+const password = document.getElementById("password");
+const generateBtn = document.querySelector("#generate");
+const copyPass = document.querySelector("#copy");
+
+function generate(){
+  // const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // const passwordLength = 6;
+  // const password = "";
+  // for (var i = 0; i <= passwordLength; i++) {
+  //   var randomNumber = Math.floor(Math.random() * chars.length);
+  //   password += chars.substring(randomNumber, randomNumber +1);
+  //  }
+  //        document.getElementById("password").value = password;
+  }
+
+
+function copy(){
+  copyPass.select();
+  navigator.clipboard.writeText(copyPass.value);
+  alert("Copied the text")
+}
+
 router.get('/generator', (req, res) => {
   res.render('generator')
 });
 
-// router.get('/id', (req, res) => {
-//   const templateVars =
-//   res.render('add', templateVar)
-// })
+
+
 
 module.exports = router;
