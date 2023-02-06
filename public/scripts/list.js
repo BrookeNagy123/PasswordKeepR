9124c67ec38f94b43495ef295fe07a448ccb3a54
binding1 <jaey.pro@gmail.com>
2023-02-06 05:09:28 +0000

    add list style for dropdown menu

--- public/scripts/list.js
+++ public/scripts/list.js
@@ -3,9 +3,27 @@ $(document).ready(function() {
     $("[id^='copy-password']").on("click", (e => {
       const passwordIdTarget = String(e.target.id);
       const passwordId = passwordIdTarget.replace('copy-password-', '');
-      const passText = $("#password-text-"+passwordId).text();
+      const passText = $("#password-text-" + passwordId).text();
       navigator.clipboard.writeText(passText);
     alert("Password copied to clipboard!");
   })
+  )}
+);
+
+
+$(document).ready(function() {
+  $("[id^='dropbtn']").on("click", (e => {
+    const buttonIdTarget = String(e.currentTarget.id)
+    const buttonId = buttonIdTarget.replace('dropbtn-', '')
+    $(".dropdown-content").css("display", "none")
+    $("#dropdown-" + buttonId).css("display", "block")
+
+    $(window).on("click", ev => {
+      if (!ev.target.matches("[id^='dropbtn']")) {
+        $("#dropdown-" + buttonId).css("display", "none")
+      }
+    });
+
+    })
   )}
 );
