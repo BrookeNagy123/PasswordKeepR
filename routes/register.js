1f7d646ee4e749eddf0e8380ac2ad3940387b7b8
Di Lu <brotherludi@hotmail.com>
2023-02-06 21:05:17 -0700

    Check if user already exist when register

--- routes/register.js
+++ routes/register.js
@@ -1,24 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const { addUser } = require('../db/queries/register');
+const { getUserWithEmail } = require('../db/queries/login');
 const db = require('../db/connection');
 
+const register = function (email) {
+  return getUserWithEmail(email)
+    .then(user => {
+      return user;
+    });
+}
+
 router.get('/', (req, res) => {
-  const templateVars = {user: req.session.email ? req.session.email : null}
+  const templateVars = { user: req.session.email ? req.session.email : null }
   res.render('register', templateVars);
 });
 
 router.post("/", (req, res) => {
-  newUser = req.body;
-  addUser(newUser)
+  const email = req.body.email;
+  register(email)
     .then(user => {
-      req.session.email = user[0].email;
-      res.redirect("/list");
-    })
-    .catch(error => {
-      console.error(error);
-      res.send(error);
+      if (user) {
+        res.send("User already exist!");
+        return;
+      };
+      newUser = req.body;
+      addUser(newUser)
+        .then(user => {
+          req.session.email = user[0].email;
+          res.redirect("/list");
+        })
+        .catch(error => {
+          console.error(error);
+          res.send(error);
+        });
     });
-});
+})
 
 module.exports = router;
