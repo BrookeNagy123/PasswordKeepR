c3d9827f1a48ff895f8ead8fed66cc823a73f488
Di Lu <brotherludi@hotmail.com>
2023-02-06 22:08:08 -0700

    Register non-exist Org show alert

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { addUser } = require('../db/queries/register');
+const { addUser, getOrgWithId } = require('../db/queries/register');
 const { getUserWithEmail } = require('../db/queries/login');
 const db = require('../db/connection');
 
@@ -11,6 +11,13 @@ const register = function (email) {
     });
 }
 
+const registerOrg = function (id) {
+  return getOrgWithId(id)
+    .then(org => {
+      return org;
+    });
+}
+
 router.get('/', (req, res) => {
   const templateVars = { user: req.session.email ? req.session.email : null }
   res.render('register', templateVars);
@@ -18,23 +25,31 @@ router.get('/', (req, res) => {
 
 router.post("/", (req, res) => {
   const email = req.body.email;
+  const id = req.body.organization_id
   register(email)
     .then(user => {
       if (user) {
         res.status(400).send('🚨Email is Already Registered!😳');
         return;
       };
-      newUser = req.body;
-      addUser(newUser)
-        .then(user => {
-          req.session.email = user[0].email;
-          res.redirect("/list");
-        })
-        .catch(error => {
-          console.error(error);
-          res.send(error);
+      // registerOrg(id)
+      //   .then(org => {
+      //     if (!org) {
+      //       res.status(400).send('Organizations does not exist!');
+      //       return;
+      //     };
+          newUser = req.body;
+          addUser(newUser)
+            .then(user => {
+              req.session.email = user[0].email;
+              res.redirect("/list");
+            })
+            .catch(error => {
+              console.error(error);
+              res.send(error);
+            });
         });
-    });
-})
+    })
+// })
 
 module.exports = router;
